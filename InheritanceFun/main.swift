5b70caf4fe4c67f99969b44f8450049e5036e9b3
Murphy-Beach, August J <amurphy-beach@hk223-27721.local>
2018-09-13 10:12:12 -0700

    Initial Commit

---
+++ InheritanceFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InheritanceFun
+//
+//  Created by Murphy-Beach, August J on 9/13/18.
+//  Copyright © 2018 Murphy-Beach, August J. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
